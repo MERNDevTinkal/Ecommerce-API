872d703e97f072682b753888e506a82c8f744012
Tinkal Kumar <126617806+MERNDevTinkal@users.noreply.github.com>
2024-02-22 10:10:18 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+// connecting mongoose to its default server and ecommerceDB
+mongoose.connect('mongodb+srv://soumyasri2245:Soumya22%4034@cluster0.u2ywt3o.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+module.exports = db;
